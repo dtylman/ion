e1847a61c2e8f4a6bb9bdec503d5a451e9e15ae5
danny_tylman <dtylman@users.noreply.github.com>
2015-01-21 00:24:33 -0800

    windows poco build

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+rem call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
 cd lib\poco-1.6.0-all
-call buildwin 100 build shared both Win32 nosamples notests msbuild
+call buildwin 120 build static_mt both Win32 nosamples notests msbuild
 cd ..
 
