161d0af88511dee8f724f02f84ccc1fab8a97048
danny_tylman <dtylman@users.noreply.github.com>
2015-01-28 08:35:52 -0800

    development

---
+++ mabat/mabat/FormMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace mabat
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            this.tabPageDevices.Controls.Add(new DevicesControl());
+        }
+     
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.ShowDialog(this);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataModel.Instance();
+        }
+
+        
+        
+    }
+}
