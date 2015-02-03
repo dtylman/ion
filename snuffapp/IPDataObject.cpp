d658cc8df1896fa4ee0e3eea9e1d9b166286fade
danny_tylman <dtylman@users.noreply.github.com>
2015-02-03 04:54:05 -0800

    development

--- snuffapp/IPDataObject.cpp
+++ snuffapp/IPDataObject.cpp
@@ -101,8 +101,8 @@ void IPDataObject::findAll(IPData::Container& container)
 void IPDataObject::findOnline(IPData::Container& container)
 {
     Poco::Timespan interval = Poco::Timespan::MINUTES * Poco::Util::Application::instance().config().getUInt("ion.offline-interval", 10);
-    Poco::Timestamp now;
-    now -= interval;
-	std::time_t last = now.epochTime();
-    _session << "SELECT mac, ip, last_seen,iface FROM ip WHERE last_seen>?", use(last), into(container), now;
+    Poco::Timestamp ts;
+    ts -= interval;
+	std::time_t last = ts.epochTime();
+	_session << "SELECT mac, ip, last_seen,iface FROM ip WHERE last_seen>?", use(last), into(container), now;
 }
