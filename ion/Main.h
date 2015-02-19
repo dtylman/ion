c971e24f4c42fbcdaf2152bbef5f0d2db3e79c20
danny_tylman <dtylman@users.noreply.github.com>
2015-02-19 17:55:18 +0200

    web framework

--- ion/Main.h
+++ ion/Main.h
@@ -10,6 +10,8 @@
 
 #include <Poco/Util/ServerApplication.h>
 #include <Poco/Util/Timer.h>
+#include <Poco/Util/PropertyFileConfiguration.h>
+#include <Poco/AutoPtr.h>
 #include "Solicitator.h"
 
 class Main : public Poco::Util::ServerApplication
@@ -21,11 +23,11 @@ public:
 protected:
     virtual int main(const std::vector<std::string>& args);
     void initialize(Poco::Util::Application& self);
-    int loadConfiguration(int priority = PRIO_DEFAULT);
 private:
     void onOnlineScan(Poco::Util::TimerTask& timerTask);
     Poco::Util::Timer _timer;
     Solicitator _solicitator;
+    Poco::AutoPtr<Poco::Util::PropertyFileConfiguration> _config;
 };
 
 #endif	/* ZAKIFAPP_H */
