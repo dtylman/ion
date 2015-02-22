04801f3525b135533092227414ee8f90d305196f
danny_tylman <dtylman@users.noreply.github.com>
2015-02-22 16:54:35 +0200

    styling

--- ion/EventConfigPage.cpp
+++ ion/EventConfigPage.cpp
@@ -30,11 +30,13 @@ bool EventConfigPage::handleForm(Poco::Net::HTMLForm& form, Poco::Net::HTTPServe
 
 void EventConfigPage::renderBody(std::ostream& output, Poco::Net::HTTPServerRequest& request)
 {
-    output << "<form class='form-group'>";
+    output << "<form>";
     output << "<div class='panel panel-default'>";
+
     output << "<div class='panel-heading'>";
     output << "<h3 class='panel-title'>This should explain what this page is for</h3>";
     output << "</div>";
+
     output << "<div class='panel-body'>";
     renderPanel(output, EventNotification::IP_OFFLINE);
     renderPanel(output, EventNotification::IP_ONLINE);
@@ -42,10 +44,11 @@ void EventConfigPage::renderBody(std::ostream& output, Poco::Net::HTTPServerRequ
     renderPanel(output, EventNotification::THING_OFFLINE);
     renderPanel(output, EventNotification::THING_ONLINE);
     output << "</div>";
-    output << "</div>";
+
+    output << "<div class='panel-footer clearfix'>";
     output << "<input class='btn btn-success' type='submit' value='Save' >";
     output << Poco::format(" <a href='%s' class='btn btn-primary'>Cancel</a>", ThingsPage::Link);
-
+    output << "</div>";
     output << "</div>";
 
     output << "</form>";
@@ -58,16 +61,20 @@ std::string EventConfigPage::title() const
 
 void EventConfigPage::renderPanel(std::ostream& output, const std::string& eventName)
 {
+    std::string keyName = Poco::format("ion.events.%s.text", eventName);
+    std::string text = Poco::Util::Application::instance().config().getString(keyName);
 
-    output << "<div class='row'>";
-    output << "<div class='col-lg-6'>";
-    output << "When " << eventName << ": ";
-    output << "<div class='input-group'>";
+    output << "<div class='row form-group'>";
+    output << Poco::format("<label class='col-xs-2'>When %s:</label>", eventName);
+    output << "<label class='control-label col-xs-1' >Message Text:</label>";
+    output << "<div class='col-xs-6'>";
+    output << Poco::format("<input class='form-control' name='txt%s' value='%s'>", eventName, text);
+    output << "</div>";
+    output << "</div>";
+    output << "<div class='row form-group'>";
     renderCheckbox(output, eventName, "save", "Save to local database");
     renderCheckbox(output, eventName, "mail", "Send Mail");
     renderCheckbox(output, eventName, "syslog", "Write to system logger");
-    output << "</div>";
-    output << "</div>";
     output << "</div>";
 
 }
@@ -76,11 +83,12 @@ void EventConfigPage::renderCheckbox(std::ostream& output, const std::string& ev
 {
     std::string keyName = Poco::format("ion.events.%s.%s", eventName, eventType);
     std::string checeked;
-    if (Poco::Util::Application::instance().config().getBool(keyName, false)) {
+    if (Poco::Util::Application::instance().config().getBool(keyName)) {
         checeked = "checked";
     }
-    output << "<span class='input-group-addon'>";
-    output << Poco::format("<input type='checkbox' name='chk%s_%s' %s>", eventType, eventName, checeked);
-    output << "</span>";
-    output << Poco::format("<label class='form-control'>%s</label>", desc);
+    output << "<div class='col-xs-3'>";
+    output << Poco::format("<input type='checkbox' name='chk%s_%s' %s value='%s'>", eventType, eventName, checeked, desc);
+    output << "</div>";
+
 }
+
