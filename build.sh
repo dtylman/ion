c2f2a475432805f43e461e938c37ac2cf43fc7f5
danny_tylman <danny_tylman@localhost>
2015-02-24 17:00:13 +0200

    development

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd lib/poco-1.6.0-all
-./configure --static --no-tests --no-samples --omit=Data/MySQL,Data/ODBC,Zip,MongoDB,NetSSL,NetSSL_OpenSSL
+./configure --static --no-tests --no-samples --omit=Data/MySQL,Data/ODBC,Zip,MongoDB --sqlite-thread-safe=2
 make -j4
 
