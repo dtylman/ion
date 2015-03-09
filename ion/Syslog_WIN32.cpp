d44ece7d3aabd244e42f512bfb40a9d533195b7c
danny_tylman <dtylman@users.noreply.github.com>
2015-03-09 22:09:49 +0200

    adding syslog

--- ion/Syslog_WIN32.cpp
+++ ion/Syslog_WIN32.cpp
@@ -7,7 +7,7 @@
 
 #include "Syslog.h"
 
-Syslog::Syslog()
+Syslog::Syslog() : _logger(Poco::Logger::get("Syslog"))
 {
 }
 
@@ -17,5 +17,17 @@ Syslog::~Syslog()
 
 void Syslog::log(const EventData& eventData)
 {
-
+	HANDLE eventLog = RegisterEventSource(0, "ion");
+	if (eventLog == 0)
+	{
+		_logger.error("Cannot register event source. Last error: %u", (unsigned)GetLastError());
+		return;
+	}
+	std::string message = eventData.toString();
+	const char* lines = message.c_str();
+	if (FALSE == ReportEvent(eventLog, EVENTLOG_WARNING_TYPE, 1, 1, NULL, 1, 0, &lines, NULL))
+	{
+		_logger.error("Failed to write message to event log. Last error: %u", (unsigned)GetLastError());
+	}
+	DeregisterEventSource(eventLog);
 }
